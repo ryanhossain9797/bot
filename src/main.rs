d2e5b93272b8a1ae4da9b7a83811edfdc2688e1f
Raiyan Hossain <ryanhossain9797@gmail.com>
2021-05-02 02:00:17 +0600

    first commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use yeelight::{Bulb, CfAction, Effect, FlowExpresion, FlowTuple, Mode, Power};
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let my_bulb_ip = "192.168.88.96";
+    let mut bulb = Bulb::connect(my_bulb_ip, 55443).await?;
+
+    // Turn on the bulb
+    let response = bulb
+        .set_power(Power::On, Effect::Sudden, 0, Mode::Normal)
+        .await?;
+    println!("response: {:?}", response);
+
+    // Define flow array
+    let flow = FlowExpresion(vec![
+        FlowTuple::ct(500, 3000, 100),
+        FlowTuple::sleep(1500),
+        FlowTuple::ct(500, 5000, 100),
+        FlowTuple::sleep(1500),
+        FlowTuple::ct(500, 2600, 100),
+        FlowTuple::sleep(1500),
+    ]);
+    // Send flow command
+    let response = bulb.start_cf(10, CfAction::Stay, flow).await?;
+    println!("response: {:?}", response);
+    Ok(())
+}
