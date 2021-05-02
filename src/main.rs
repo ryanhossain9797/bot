f97f0cbf7c1be0af87a2299e1aa43e663182e9bf
Raiyan Hossain <ryanhossain9797@gmail.com>
2021-05-02 20:06:31 +0600

    control bulb

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,71 @@
-use yeelight::{Bulb, CfAction, Effect, FlowExpresion, FlowTuple, Mode, Power};
+use futures::StreamExt;
+use std::env;
+use telegram_bot::*;
+use yeelight::{
+    AdjustAction, Bulb, CfAction, Effect, FlowExpresion, FlowTuple, Mode, Power, Properties,
+    Property,
+};
 
 #[async_std::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv::dotenv()?;
     let my_bulb_ip = "192.168.88.96";
     let mut bulb = Bulb::connect(my_bulb_ip, 55443).await?;
 
-    // Turn on the bulb
-    let response = bulb
-        .set_power(Power::On, Effect::Sudden, 0, Mode::Normal)
-        .await?;
-    println!("response: {:?}", response);
+    let token = env::var("TELEGRAM_TOKEN").expect("Token not present");
+    let api = Api::new(token);
 
-    // Define flow array
-    let flow = FlowExpresion(vec![
-        FlowTuple::ct(500, 3000, 100),
-        FlowTuple::sleep(1500),
-        FlowTuple::ct(500, 5000, 100),
-        FlowTuple::sleep(1500),
-        FlowTuple::ct(500, 2600, 100),
-        FlowTuple::sleep(1500),
-    ]);
-    // Send flow command
-    let response = bulb.start_cf(10, CfAction::Stay, flow).await?;
-    println!("response: {:?}", response);
+    // Fetch new updates via long poll method
+    let mut stream = api.stream();
+    while let Some(update) = stream.next().await {
+        // If the received update contains a new message...
+        let update = update?;
+        if let UpdateKind::Message(message) = update.kind {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                // Print received text message to stdout.
+                println!("<{}>: {}", &message.from.first_name, data);
+                if &message.from.first_name == "zireael9797" {
+                    let props = Properties(vec![Property::Power]);
+
+                    let state = bulb.get_prop(&props).await?;
+
+                    println!("state is {:?}", state);
+                    if let Some(props) = state {
+                        if props.contains(&"on".to_string()) {
+                            api.send(message.chat.text(format!(
+                                "Greetings, {}. Turning light off",
+                                &message.from.first_name
+                            )))
+                            .await?;
+
+                            let response = bulb
+                                .set_power(Power::Off, Effect::Smooth, 1, Mode::Normal)
+                                .await?;
+                            println!("response: {:?}", response);
+                        } else {
+                            api.send(message.chat.text(format!(
+                                "Greetings, {}. Turning light on",
+                                &message.from.first_name
+                            )))
+                            .await?;
+
+                            bulb.set_power(Power::On, Effect::Smooth, 3, Mode::CT)
+                                .await?;
+
+                            bulb.set_bright(50, Effect::Smooth, 3).await?;
+                            bulb.set_ct_abx(3500, Effect::Smooth, 3).await?;
+                        }
+                    }
+                } else {
+                    api.send(message.text_reply(format!(
+                        "Greetings, {}. I am a private bot and you are not authorized to interact with me",
+                        &message.from.first_name
+                    )))
+                    .await?;
+                }
+            }
+        }
+    }
+
     Ok(())
 }
