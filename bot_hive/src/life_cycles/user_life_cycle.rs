68f05a48fe238bbcd6603996add937012cb6d6a6
Raiyan Hossain <ryanhossain9797@gmail.com>
2025-06-01 00:33:23 +0600

    Remove wrapper and define the lifecycle directyle

--- bot_hive/src/life_cycles/user_life_cycle.rs
+++ bot_hive/src/life_cycles/user_life_cycle.rs
@@ -11,62 +11,54 @@ use serenity::all::CreateMessage;
 type UserTransitionResult = TransitionResult<User, UserAction>;
 type UserExternalOperation = ExternalOperation<UserAction>;
 
-async fn user_transition(
-    env: Arc<Env>,
-    user_id: UserId,
-    user: User,
-    action: &UserAction,
-) -> UserTransitionResult {
-    match action {
-        UserAction::Poke => {
-            println!("Poked");
-            Ok((
-                User {
-                    maybe_poke_at: None,
-                    ..user
-                },
-                Vec::new(),
-            ))
-        }
-        UserAction::NewMessage {
-            msg,
-            start_conversation,
-        } => {
-            let mut external = Vec::<UserExternalOperation>::new();
-
-            external.push(Box::pin(placeholder_handle_bot_message(
-                env.clone(),
-                user_id.clone(),
-                msg.to_string(),
-            )));
-
-            let user = User {
-                action_count: user.action_count + 1,
-                maybe_poke_at: None, //replace with managed time,
-            };
-
-            println!("Id: {0} {1}", user_id.1, user.action_count);
-
-            Ok((user, external))
-        }
-        UserAction::SendResult(send_result) => Ok((
-            User {
-                maybe_poke_at: Some(Utc::now() + ChronoDuration::milliseconds(10_000)),
-                ..user
-            },
-            Vec::new(),
-        )),
-    }
-}
-
-pub fn user_transition_wrapper(
+pub fn user_transition(
     env: Arc<Env>,
     user_id: UserId,
     user: User,
     action: &UserAction,
 ) -> Pin<Box<dyn Future<Output = UserTransitionResult> + Send + '_>> {
-    let fut = user_transition(env, user_id, user, action);
-    Box::pin(fut)
+    Box::pin(async move {
+        match action {
+            UserAction::Poke => {
+                println!("Poked");
+                Ok((
+                    User {
+                        maybe_poke_at: None,
+                        ..user
+                    },
+                    Vec::new(),
+                ))
+            }
+            UserAction::NewMessage {
+                msg,
+                start_conversation,
+            } => {
+                let mut external = Vec::<UserExternalOperation>::new();
+
+                external.push(Box::pin(placeholder_handle_bot_message(
+                    env.clone(),
+                    user_id.clone(),
+                    msg.to_string(),
+                )));
+
+                let user = User {
+                    action_count: user.action_count + 1,
+                    maybe_poke_at: None, //replace with managed time,
+                };
+
+                println!("Id: {0} {1}", user_id.1, user.action_count);
+
+                Ok((user, external))
+            }
+            UserAction::SendResult(send_result) => Ok((
+                User {
+                    maybe_poke_at: Some(Utc::now() + ChronoDuration::milliseconds(10_000)),
+                    ..user
+                },
+                Vec::new(),
+            )),
+        }
+    })
 }
 
 pub fn schedule(user: &User) -> Vec<Scheduled<UserAction>> {
