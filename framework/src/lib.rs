76bf6272b8bdd79715c30b2312d8742cd0ca585f
Raiyan Hossain <ryanhossain9797@protonmail.com>
2026-01-07 20:05:02 +0000

    rename stuff and add recall tool

---
+++ framework/src/lib.rs
@@ -0,0 +1,204 @@
+mod bee_handle;
+mod life_cycle_handle;
+
+use bee_handle::{new_entity, Handle};
+use chrono::{DateTime, Utc};
+pub use life_cycle_handle::*;
+use std::time::Duration;
+use std::{future::Future, pin::Pin, sync::Arc};
+
+use tokio::sync::mpsc::{Receiver, Sender};
+use tokio::task::JoinHandle;
+
+pub type TransitionResult<Type, Action> =
+    anyhow::Result<(Type, Vec<Pin<Box<dyn Future<Output = Action> + Send>>>)>;
+
+pub type ExternalOperation<Action> = Pin<Box<dyn Future<Output = Action> + Send>>;
+
+pub trait LifeCycleItem: Send + Sync + Clone {}
+
+impl<T: Send + Sync + Clone> LifeCycleItem for T {}
+
+/// Extension of LifeCycleItem that supports persistence via JSON serialization
+pub trait PersistedLifeCycleItem: LifeCycleItem + serde::Serialize {}
+
+impl<T> PersistedLifeCycleItem for T where T: LifeCycleItem + serde::Serialize {}
+
+#[derive(Clone)]
+pub struct Transition<Id, State, Action, Env>(
+    pub  fn(
+        Arc<Env>,
+        Id,
+        State,
+        &Action,
+    ) -> Pin<Box<dyn Future<Output = TransitionResult<State, Action>> + Send + '_>>,
+);
+
+#[derive(Clone)]
+pub struct Scheduled<Action> {
+    pub at: DateTime<Utc>,
+    pub action: Action,
+}
+
+#[derive(Clone)]
+pub struct Schedule<State, Action>(pub fn(&State) -> Vec<Scheduled<Action>>);
+
+pub enum Activity<Action: PersistedLifeCycleItem + 'static> {
+    LifeCycleAction(Action),
+    ScheduledWakeup,
+    DeleteSelf,
+}
+
+async fn run_entity<
+    Id: PersistedLifeCycleItem + Ord + 'static,
+    State: PersistedLifeCycleItem + Default + 'static,
+    Action: PersistedLifeCycleItem + std::fmt::Debug + 'static,
+    Env: LifeCycleItem + 'static,
+>(
+    env: Arc<Env>,
+    id: Id,
+    mut receiver: Receiver<Activity<Action>>,
+    handle: LifeCycleHandle<Id, Action>,
+    transition: Transition<Id, State, Action, Env>,
+    schedule: Schedule<State, Action>,
+    self_sender: Sender<Activity<Action>>,
+) {
+    let mut state = State::default();
+    let mut maybe_scheduled: Option<JoinHandle<()>> = None;
+
+    while let Some(activity) = receiver.recv().await {
+        let now = Utc::now();
+
+        let activity_str = match &activity {
+            Activity::LifeCycleAction(action) => format!("Action: {:?}", action),
+            Activity::ScheduledWakeup => "ScheduledWakeup".to_string(),
+            Activity::DeleteSelf => "DeleteSelf".to_string(),
+        };
+        println!(
+            "TRANSITION AT {now} - Lifecycle: {} - {}",
+            std::any::type_name::<State>(),
+            activity_str
+        );
+        match activity {
+            Activity::LifeCycleAction(action) => {
+                match transition.0(env.clone(), id.clone(), state.clone(), &action).await {
+                    Ok((updated_user, external)) => {
+                        match &maybe_scheduled {
+                            Some(scheduled) => {
+                                scheduled.abort();
+                            }
+                            None => {}
+                        }
+                        let mut scheduled = schedule.0(&updated_user);
+
+                        scheduled.sort_by_key(|scheduled| scheduled.at);
+
+                        let earliest = scheduled.into_iter().next();
+
+                        match earliest {
+                            Some(scheduled) => {
+                                let self_sender = self_sender.clone();
+                                let timer_handle = tokio::spawn(async move {
+                                    let sleep_for = scheduled.clone().at - now;
+                                    match sleep_for.to_std() {
+                                        Ok(sleep_duration) => {
+                                            tokio::time::sleep(sleep_duration).await;
+                                            while Utc::now() < scheduled.at {
+                                                tokio::time::sleep(Duration::from_millis(10)).await;
+                                            }
+                                            let _ = self_sender
+                                                .clone()
+                                                .send(Activity::ScheduledWakeup)
+                                                .await;
+                                        }
+                                        Err(_negative_time_error) => {
+                                            // Negative duration means the scheduled time has already passed
+                                            let _ = self_sender
+                                                .clone()
+                                                .send(Activity::ScheduledWakeup)
+                                                .await;
+                                        }
+                                    }
+                                });
+
+                                maybe_scheduled = Some(timer_handle)
+                            }
+                            None => {}
+                        }
+
+                        external.into_iter().for_each(|f| {
+                            let handle: LifeCycleHandle<Id, Action> = handle.clone();
+                            let user_id = id.clone();
+                            tokio::spawn(async move {
+                                let action = f.await;
+                                handle.act(user_id, action).await;
+                            });
+                        });
+                        state = updated_user;
+                    }
+                    Err(_) => (),
+                }
+            }
+            Activity::ScheduledWakeup => {
+                let mut scheduled = schedule.0(&state);
+                scheduled.sort_by_key(|scheduled| scheduled.at);
+
+                let earliest = scheduled.into_iter().next();
+
+                match earliest {
+                    Some(scheduled) => {
+                        let sleep_for = scheduled.at - now;
+
+                        match sleep_for.to_std() {
+                            Ok(_time_left) => {
+                                println!("Not Ready"); //TODO handle unexpected wakeup
+                            }
+                            Err(_negative_time_error) => {
+                                // Negative duration means the scheduled time has already passed
+                                let _ = self_sender
+                                    .send(Activity::LifeCycleAction(scheduled.action))
+                                    .await;
+                            }
+                        }
+                    }
+                    None => {}
+                }
+            }
+            Activity::DeleteSelf => todo!(),
+        }
+    }
+}
+
+async fn start_life_cycle<
+    Id: PersistedLifeCycleItem + Ord + 'static,
+    State: PersistedLifeCycleItem + Default + 'static,
+    Action: PersistedLifeCycleItem + std::fmt::Debug + 'static,
+    Env: LifeCycleItem + 'static,
+>(
+    env: Arc<Env>,
+    life_cycle_handle: LifeCycleHandle<Id, Action>,
+    mut receiver: Receiver<(Id, Action)>,
+    transition: Transition<Id, State, Action, Env>,
+    schedule: Schedule<State, Action>,
+) -> ! {
+    let mut handle_by_id = std::collections::BTreeMap::<Id, Handle<Action>>::new();
+
+    while let Some((id, action)) = receiver.recv().await {
+        match handle_by_id.contains_key(&id) {
+            true => (),
+            false => {
+                let handle = new_entity(
+                    env.clone(),
+                    id.clone(),
+                    life_cycle_handle.clone(),
+                    transition.clone(),
+                    schedule.clone(),
+                );
+                handle_by_id.insert(id.clone(), handle.clone());
+            }
+        }
+        let handle = handle_by_id[&id].clone();
+        tokio::spawn(async move { handle.act(action).await });
+    }
+    panic!()
+}
